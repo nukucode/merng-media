22cf49ef2c8387f5dd2c89ee9f94a0675d630987
therogersak <therogersak@gmail.com>
2023-01-27 20:34:59 +0530

    🛂 authorization

---
+++ utils/check-auth.js
@@ -0,0 +1,23 @@
+import { AuthenticationError } from "apollo-server";
+
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "../config.js";
+
+export default (context) => {
+  // context = { ... headers }
+  const authHeader = context.req.headers.authorization;
+  if (authHeader) {
+    // Bearer ....
+    const token = authHeader.split("Bearer ")[1];
+    if (token) {
+      try {
+        const user = jwt.verify(token, SECRET_KEY);
+        return user;
+      } catch (err) {
+        throw new AuthenticationError("Invalid/Expired token");
+      }
+    }
+    throw new Error("Authentication token must be 'Bearer [token]");
+  }
+  throw new Error("Authorization header must be provided");
+};
